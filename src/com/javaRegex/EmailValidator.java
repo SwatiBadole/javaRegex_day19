7dde261224fbd07655478822f1137f1f0d16ae48
SwatiBadole <swatibadole1997@gmail.com>
2022-04-24 14:53:33 -0700

    added:git ignore

--- src/com/javaRegex/EmailValidator.java
+++ src/com/javaRegex/EmailValidator.java
@@ -1,6 +1,14 @@
 package com.javaRegex;
 
+import java.util.regex.*;
+
 public class EmailValidator {
+    	
+	   private Pattern pattern;
+       private Matcher matcher;
+       
+      private static final String EMAIL_PATTERN
+      "^[_A-za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +"[A-Za-z0-9-]+(\\.[])"
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
